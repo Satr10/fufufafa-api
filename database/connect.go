670f00273874f0e029b435b60fb80c1f47795423
Satrio <personal.satrio@protonmail.com>
2025-04-12 20:24:20 +0700

    first commit

---
+++ database/connect.go
@@ -0,0 +1,20 @@
+package database
+
+import (
+	"fmt"
+
+	"github.com/Satr10/fufufafa-api/model"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func ConnectDB() {
+	var err error
+	DB, err = gorm.Open(sqlite.Open("fufufafa.db"), &gorm.Config{})
+	if err != nil {
+		panic("gagal connect ke database")
+	}
+	fmt.Println("Connection Opened to Database")
+	DB.AutoMigrate(&model.Post{})
+	fmt.Println("Database Migrated")
+}
