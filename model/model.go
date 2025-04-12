28775a726d4b97b79b9b987fe225bd0040e011f7
Satrio <personal.satrio@protonmail.com>
2025-04-12 20:42:21 +0700

    moved to /api

--- model/model.go
+++
@@ -1,9 +0,0 @@
-package model
-
-type Post struct {
-	ID       int    `gorm:"primaryKey;column:id" json:"id"`
-	Content  string `gorm:"type:text" json:"content"`
-	Datetime string `gorm:"index" json:"datetime"`
-	Doksli   string `json:"doksli"`
-	ImageURL string `gorm:"column:image_url" json:"image_url"`
-}
